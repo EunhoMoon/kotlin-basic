6f255f42db91d741f43ca765129f18fc23424a2a
eunho.moon@gmail.com <eunho.moon@gmail.com>
2023-02-10 17:53:48 +0900

    lesson04 - 연산자

---
+++ src/main/kotlin/lesson04/Lesson04Main.kt.kt
@@ -0,0 +1,14 @@
+package lesson04
+
+import `object`.JavaMoney
+
+fun main() {
+
+    val money1 = JavaMoney(2_000L)
+    val money2 = JavaMoney(1_000L)
+
+    if (money1 > money2) {
+        println("Money1이 Money2보다 금액이 큽니다.")
+    }
+
+}
