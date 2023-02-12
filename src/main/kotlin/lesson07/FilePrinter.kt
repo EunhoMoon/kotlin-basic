8253472b6c37ee24b6f7d851b87e69ec071eb13c
Janek <eunho.moon@gmail.com>
2023-02-12 20:47:35 +0900

    lesson07 - 예외

---
+++ src/main/kotlin/lesson07/FilePrinter.kt
@@ -0,0 +1,27 @@
+package lesson07
+
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileReader
+
+fun main() {
+    val printer: FilePrinter = FilePrinter()
+
+    printer.readFile()
+}
+
+class FilePrinter {
+    fun readFile() {
+        val currentFile = File(".")
+        val file = File(currentFile.absolutePath + "/a.txt")
+        val reader = BufferedReader(FileReader(file))
+        println(reader.readLine())
+        reader.close()
+    }
+
+    fun readFile(path: String) {
+        BufferedReader(FileReader(path)).use { reader ->
+            println(reader.readLine())
+        }
+    }
+}
