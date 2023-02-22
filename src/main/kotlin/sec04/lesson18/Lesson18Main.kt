4df3f6b483db4f531760265b0b3e92c7724413fd
Janek <eunho.moon@gmail.com>
2023-02-22 21:22:36 +0900

    lesson18 - 람다

---
+++ src/main/kotlin/sec04/lesson18/Lesson18Main.kt
@@ -0,0 +1,43 @@
+package sec04.lesson18
+
+import `object`.Fruit
+import java.util.logging.Filter
+
+fun main() {
+    val fruits = listOf(
+        Fruit("사과", 1_000),
+        Fruit("사과", 1_200),
+        Fruit("사과", 1_200),
+        Fruit("사과", 1_500),
+        Fruit("바나나", 3_000),
+        Fruit("바나나", 3_200),
+        Fruit("바나나", 2_500),
+        Fruit("수박", 10_000)
+    )
+
+    val isApple: (Fruit) -> Boolean = fun(fruit: Fruit): Boolean {
+        return fruit.name == "사과"
+    }
+
+    val isApple2: (Fruit) -> Boolean = { fruit: Fruit -> fruit.name == "사과" }
+
+    isApple(fruits[0])
+    isApple.invoke(fruits[0])
+
+    filterFruits(fruits, isApple)
+    filterFruits(fruits, isApple2)
+    filterFruits(fruits) { it.name == "사과" }
+
+}
+
+private fun filterFruits(
+    fruits: List<Fruit>, filter: (Fruit) -> Boolean)
+        : List<Fruit> {
+    val results = mutableListOf<Fruit>()
+    for (fruit in fruits) {
+        if (filter(fruit)) {
+            results.add(fruit)
+        }
+    }
+    return results
+}
