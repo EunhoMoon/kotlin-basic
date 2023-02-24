bf3bf25a0f3038b9af7c32f60a99a19ff2d02ce4
Janek <eunho.moon@gmail.com>
2023-02-24 21:27:10 +0900

    lesson19 - alias, component, take if

--- src/main/kotlin/sec05/lesson19/Lesson19Main.kt
+++ src/main/kotlin/sec05/lesson19/Lesson19Main.kt
@@ -1,15 +1,24 @@
 package sec05.lesson19
 
-import sec04.lesson18.Fruit
+import sec04.lesson18.Fruit as FruitAlias    // as import
 
 fun main() {
-
+    val person = Person("Eunho", 31)
+    val (name, age) = person
+//    val name = person.component1()
+//    val age = person.component2()
+    println("이름 : ${name}, 나이 : ${age}")    // 구조분해 할당 -> data class, component 함수 필요
 }
 
-typealias FruitFilter = (Fruit) -> Boolean
+typealias FruitFilter = (FruitAlias) -> Boolean  // type alias
 private fun filterFruits(
-    fruits: List<Fruit>, filter: FruitFilter)
-        : List<Fruit> {
-    val results = mutableListOf<Fruit>()
+    fruits: List<FruitAlias>, filter: FruitFilter)
+        : List<FruitAlias> {
+    val results = mutableListOf<FruitAlias>()
     return fruits.filter(filter)
 }
+
+data class Person(
+    val name: String,
+    val age: Int
+)
